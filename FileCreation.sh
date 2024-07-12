aad2a95b902144b7a4a8b95f81cfc68d078d440e
Siabsundar <sibasundarsahoo31@gmail.com>
2024-07-12 12:02:56 +0000

    changes in FileCreation file

--- FileCreation.sh
+++ FileCreation.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+#input filename as 1st argument 
 file_name=$1
-start=$2
-end=$3
+start=$2      #input starting range as 2nd arg
+end=$3        #input last range as 3rd arg
 
+# check if input paramiter are 3 or not 
 if [ $# -ne 3 ]; then
 	echo "Use 3 Arguments like- ./$0 1 15 . Not like $@"
 fi
